2609581811648410813d469db68c719c3d5d6a82
Pedro <pbruno1283@gmail.com>
2021-11-08 23:56:05 -0300

    Api configurada

---
+++ app/src/main/java/com/pedrobruno/mvmmnews/Source.kt
@@ -0,0 +1,6 @@
+package com.pedrobruno.mvmmnews
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/pedrobruno/mvmmnews/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.pedrobruno.mvmmnews.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "96b42b975b15482e87f5dbbd0cab01b3"
+        const val BASE_URL ="https://newsapi.org"
+    }
+}
