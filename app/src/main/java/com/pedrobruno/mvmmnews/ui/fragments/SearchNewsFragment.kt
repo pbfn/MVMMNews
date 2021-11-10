6e7c4c3d4a095588771f0e88f7671251f9a027f9
Pedro <pbruno1283@gmail.com>
2021-11-10 11:45:12 -0300

    SearchPage criada

--- app/src/main/java/com/pedrobruno/mvmmnews/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/pedrobruno/mvmmnews/ui/fragments/SearchNewsFragment.kt
@@ -1,25 +1,39 @@
 package com.pedrobruno.mvmmnews.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.pedrobruno.mvmmnews.adapters.NewsAdapter
 import com.pedrobruno.mvmmnews.databinding.FragmentSearchNewsBinding
 import com.pedrobruno.mvmmnews.ui.NewsActivity
 import com.pedrobruno.mvmmnews.ui.NewsViewModel
+import com.pedrobruno.mvmmnews.util.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import com.pedrobruno.mvmmnews.util.Resource
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
-class SearchNewsFragment:Fragment() {
+class SearchNewsFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchNewsBinding
     private lateinit var viewModel: NewsViewModel
+    private lateinit var newsAdapter: NewsAdapter
+
+    val TAG = "SearchNewsFragment"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentSearchNewsBinding.inflate(inflater,container,false)
+        binding = FragmentSearchNewsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -27,5 +41,61 @@ class SearchNewsFragment:Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+        observeData()
+        setupSearchNews()
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding.rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun observeData() {
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer { response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgessBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgessBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "Ocorreu um erro $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgessBar()
+                }
+            }
+        })
+    }
+
+    private fun setupSearchNews() {
+        var job: Job? = null
+        binding.etSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(SEARCH_NEWS_TIME_DELAY)
+                editable?.let {
+                    if (editable.toString().isNotEmpty()) {
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+            }
+        }
+    }
+
+    private fun hideProgessBar() {
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgessBar() {
+        binding.paginationProgressBar.visibility = View.VISIBLE
     }
 }
--- app/src/main/java/com/pedrobruno/mvmmnews/util/Constants.kt
+++ app/src/main/java/com/pedrobruno/mvmmnews/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "96b42b975b15482e87f5dbbd0cab01b3"
         const val BASE_URL ="https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
